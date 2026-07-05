5bf16611cf2745092e59580163acf60bc16f5feb
ryomenhaider <onlinep680@gmail.com>
2026-07-05 14:40:53 +0500

    Fred (indicators.py) completed

---
+++ src/hermes/connectors/fred/indicators.py
@@ -0,0 +1,61 @@
+FRED_INDICATORS = [
+    {
+        'code': 'GDP',
+        'series_id': 'GDP', 
+        'unit': 'Billions of Dollars',
+    },
+    
+    {
+        'code': 'Real Gross Domestic Product',
+        'series_id': 'GDPC1', 
+        'unit': 'Billions of Chained 2017 Dollars',
+    },    
+    
+    {
+        'code': 'Consumer Price Index for All Urban Consumers: All Items in U.S. City Average',
+        'series_id': 'CPIAUCSL', 
+        'unit': 'Index (1982-1984=100)',
+    },    
+    
+    {
+        'code': 'Unemployment Rate',
+        'series_id': 'UNRATE', 
+        'unit': 'Percent',
+    },    
+    
+    {
+        'code': 'Federal Funds Effective Rate',
+        'series_id': 'FEDFUNDS', 
+        'unit': 'Percent',
+    },    
+    
+    {
+        'code': 'Industrial Production: Total Index',
+        'series_id': 'INDPRO', 
+        'unit': 'Index 2017=100',
+    },    
+    
+    {
+        'code': 'Market Yield on U.S. Treasury Securities at 10-Year Constant Maturity, Quoted on an Investment Basis',
+        'series_id': 'GS10', 
+        'unit': 'Percent',
+    },    
+    
+    {
+        'code': 'Advance Retail Sales: Retail Trade and Food Services',
+        'series_id': 'RSAFS', 
+        'unit': 'Millions of Dollars',
+    },    
+    
+    {
+        'code': 'M2',
+        'series_id': 'M2SL', 
+        'unit': 'Billions of Dollars',
+    },    
+    
+    {
+        'code': 'Trade Balance: Goods and Services, Balance of Payments Basis',
+        'series_id': 'BOPGSTB', 
+        'unit': 'Millions of Dollars',
+    },
+]
